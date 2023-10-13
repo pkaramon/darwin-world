dce3e32aa3739594eebb8c31d095aa41073428e0
Piotr Karamon <piotrkaramon3@gmail.com>
2023-10-14 00:50:58 +0200

    feat(World): made run method interpret movement arguments

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -2,15 +2,23 @@ package agh.ics.oop;
 
 public class World {
     public static void main(String[] args) {
-        System.out.println("system wystartował");
+        System.out.println("Start");
         run(args);
-        System.out.println("system zakończył działanie");
+        System.out.println("Stop");
     }
 
     private static void run(String[] args) {
-        System.out.println("zwierzak idzie do przodu");
-        String arguments = String.join(", ", args);
-        System.out.println(arguments);
+        for (String arg : args) {
+            String message = switch (arg) {
+                case "f" -> "Zwierzak idzie do przodu";
+                case "b" -> "Zwierzak idzie do tyłu";
+                case "r" -> "Zwierzak skręca w prawo";
+                case "l" -> "Zwierzak skręca w lewo";
+                default -> "";
+            };
+
+            if (!message.isEmpty()) System.out.println(message);
+        }
     }
 
 }
