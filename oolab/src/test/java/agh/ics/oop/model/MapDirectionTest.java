0dff7c5aea1782ddef69d89611dfc478f3596d0d
Piotr Karamon <piotrkaramon3@gmail.com>
2023-10-23 00:50:27 +0200

    test(model): created MapDirectionTest

---
+++ oolab/src/test/java/agh/ics/oop/model/MapDirectionTest.java
@@ -0,0 +1,25 @@
+package agh.ics.oop.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MapDirectionTest {
+
+    @Test
+    public void next_MovesDirectionClockwise() {
+        assertEquals(MapDirection.EAST, MapDirection.NORTH.next());
+        assertEquals(MapDirection.SOUTH, MapDirection.EAST.next());
+        assertEquals(MapDirection.WEST, MapDirection.SOUTH.next());
+        assertEquals(MapDirection.NORTH, MapDirection.WEST.next());
+    }
+
+
+    @Test
+    public void previous_MovesMapDirectionCounterClockwise() {
+        assertEquals(MapDirection.WEST, MapDirection.NORTH.previous());
+        assertEquals(MapDirection.SOUTH, MapDirection.WEST.previous());
+        assertEquals(MapDirection.EAST, MapDirection.SOUTH.previous());
+        assertEquals(MapDirection.NORTH, MapDirection.EAST.previous());
+    }
+}
