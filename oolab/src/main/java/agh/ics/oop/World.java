e978a8e76e2d5dc60bad361d6bfeaf8534c41410
Piotr Karamon <piotrkaramon3@gmail.com>
2023-10-23 00:45:53 +0200

    feat(model): added Vector2d

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,12 +1,20 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
+
 
 public class World {
     public static void main(String[] options) {
         System.out.println("Start");
         run(OptionsParser.parse(options));
         System.out.println("Stop");
+
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
 
     private static void run(MoveDirection[] directions) {
