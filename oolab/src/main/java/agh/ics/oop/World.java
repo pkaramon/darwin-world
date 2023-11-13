9d242fbafc3352d7dae28d282d715c936e013714
Piotr Karamon <piotrkaramon3@gmail.com>
2023-11-13 02:03:15 +0100

    feat(model): World.main now runs the simulation with GrassField

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,17 +1,22 @@
 package agh.ics.oop;
 
-import agh.ics.oop.model.*;
+import agh.ics.oop.model.GrassField;
+import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class World {
     public static void main(String[] options) {
         System.out.println("Start");
-        List<MoveDirection> directions = OptionsParser.parse(Arrays.stream(options).toList());
+
+        List<MoveDirection> directions = OptionsParser.parse(List.of("f b r l f f r r f f f f f f f f".split(" ")));
+        GrassField gf = new GrassField(10);
         List<Vector2d> positions = List.of(new Vector2d(2, 2), new Vector2d(3, 4));
-        Simulation simulation = new Simulation(directions, positions, new RectangularMap(5, 5));
+        Simulation simulation = new Simulation(directions, positions,  gf);
         simulation.run();
+
+        System.out.println(gf);
         System.out.println("Stop");
 
     }
