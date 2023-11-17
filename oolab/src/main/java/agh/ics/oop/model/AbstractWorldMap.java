79d9c6fb37b9685d6a22b0bcdf1dbf63e207d3a9
Piotr Karamon <piotrkaramon3@gmail.com>
2023-11-17 16:04:44 +0100

    feat(AbstractWorldMap): added observer pattern(MapChangeListener)

--- oolab/src/main/java/agh/ics/oop/model/AbstractWorldMap.java
+++ oolab/src/main/java/agh/ics/oop/model/AbstractWorldMap.java
@@ -2,20 +2,33 @@ package agh.ics.oop.model;
 
 import agh.ics.oop.model.util.MapVisualizer;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public abstract class AbstractWorldMap implements WorldMap{
+public abstract class AbstractWorldMap implements WorldMap {
     protected final Map<Vector2d, Animal> animals = new HashMap<>();
     private final MapVisualizer mapVisualizer = new MapVisualizer(this);
+    private final Set<MapChangeListener> listeners = new LinkedHashSet<>();
 
-   abstract Boundary getCurrentBounds();
+    public void addListener(MapChangeListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeListener(MapChangeListener listener) {
+        listeners.remove(listener);
+    }
+
+    private void mapChanged(String message) {
+        for (MapChangeListener listener : listeners) {
+            listener.mapChanged(this, message);
+        }
+    }
+
+    abstract Boundary getCurrentBounds();
 
     @Override
     public String toString() {
         Boundary boundary = getCurrentBounds();
+
         return mapVisualizer.draw(boundary.lowerLeft(), boundary.upperRight());
     }
 
@@ -31,16 +44,22 @@ public abstract class AbstractWorldMap implements WorldMap{
         }
 
         this.animals.put(animal.getPosition(), animal);
+        mapChanged("Animal was placed at %s".formatted(animal.getPosition()));
     }
 
     @Override
     public void move(Animal animal, MoveDirection direction) {
+        String oldInfo = "%s, %s".formatted(animal.getPosition(), animal.toString());
+
         Vector2d oldPosition = animal.getPosition();
-        if(objectAt(oldPosition) != animal) {
-            return;
-        }
         animal.move(direction, this);
-        if(oldPosition.equals(animal.getPosition())) {
+
+        String message = "Animal changed from:  %s to %s, %s".formatted(
+                oldInfo, animal.getPosition(), animal.toString()
+        );
+        mapChanged(message);
+
+        if (oldPosition.equals(animal.getPosition())) {
             return;
         }
         this.animals.remove(oldPosition);
