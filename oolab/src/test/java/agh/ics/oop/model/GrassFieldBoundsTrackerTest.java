8643893759156356d1696aea0d20d8cea9f4f369
Piotr Karamon <piotrkaramon3@gmail.com>
2023-11-17 16:42:42 +0100

    refactor(GrassFieldBoundsTrackerTest): shortened variable names

--- oolab/src/test/java/agh/ics/oop/model/GrassFieldBoundsTrackerTest.java
+++ oolab/src/test/java/agh/ics/oop/model/GrassFieldBoundsTrackerTest.java
@@ -5,31 +5,31 @@ import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GrassFieldBoundsTrackerTest {
-    private GrassFieldBoundsTracker grassFieldsBoundsTracker;
+    private GrassFieldBoundsTracker boundsTracker;
     @BeforeEach
     public void setUp() {
-        grassFieldsBoundsTracker = new GrassFieldBoundsTracker();
+        boundsTracker = new GrassFieldBoundsTracker();
     }
 
     @Test
     public void lowerLeftCornerOfEmptyMapShouldBeZeroZero() {
-        Vector2d result = grassFieldsBoundsTracker.lowerLeft();
+        Vector2d result = boundsTracker.lowerLeft();
         assertEquals(new Vector2d(0, 0), result);
     }
 
     @Test
     public void upperRightCornerOfEmptyMapShouldBeZeroZero() {
-        Vector2d result = grassFieldsBoundsTracker.upperRight();
+        Vector2d result = boundsTracker.upperRight();
         assertEquals(new Vector2d(0, 0), result);
     }
 
     @Test
     public void addingOneElement() {
         WorldElement element = new Animal(new Vector2d(3, 5));
-        grassFieldsBoundsTracker.addElement(element);
+        boundsTracker.addElement(element);
 
-        Vector2d lowerLeft = grassFieldsBoundsTracker.lowerLeft();
-        Vector2d upperRight = grassFieldsBoundsTracker.upperRight();
+        Vector2d lowerLeft = boundsTracker.lowerLeft();
+        Vector2d upperRight = boundsTracker.upperRight();
 
         assertEquals(new Vector2d(3, 5), lowerLeft);
         assertEquals(new Vector2d(3, 5), upperRight);
@@ -39,11 +39,11 @@ class GrassFieldBoundsTrackerTest {
     public void addingTwoElements() {
         WorldElement element1 = new Grass(new Vector2d(3, 5));
         WorldElement element2 = new Animal(new Vector2d(1, 2));
-        grassFieldsBoundsTracker.addElement(element1);
-        grassFieldsBoundsTracker.addElement(element2);
+        boundsTracker.addElement(element1);
+        boundsTracker.addElement(element2);
 
-        Vector2d lowerLeft = grassFieldsBoundsTracker.lowerLeft();
-        Vector2d upperRight = grassFieldsBoundsTracker.upperRight();
+        Vector2d lowerLeft = boundsTracker.lowerLeft();
+        Vector2d upperRight = boundsTracker.upperRight();
 
         assertEquals(new Vector2d(1, 2), lowerLeft);
         assertEquals(new Vector2d(3, 5), upperRight);
@@ -54,12 +54,12 @@ class GrassFieldBoundsTrackerTest {
         WorldElement element1 = new Animal(new Vector2d(3, 5));
         WorldElement element2 = new Grass(new Vector2d(1, 2));
         WorldElement element3 = new Grass(new Vector2d(6, 8));
-        grassFieldsBoundsTracker.addElement(element1);
-        grassFieldsBoundsTracker.addElement(element2);
-        grassFieldsBoundsTracker.addElement(element3);
+        boundsTracker.addElement(element1);
+        boundsTracker.addElement(element2);
+        boundsTracker.addElement(element3);
 
-        Vector2d lowerLeft = grassFieldsBoundsTracker.lowerLeft();
-        Vector2d upperRight = grassFieldsBoundsTracker.upperRight();
+        Vector2d lowerLeft = boundsTracker.lowerLeft();
+        Vector2d upperRight = boundsTracker.upperRight();
 
         assertEquals(new Vector2d(1, 2), lowerLeft);
         assertEquals(new Vector2d(6, 8), upperRight);
@@ -68,12 +68,12 @@ class GrassFieldBoundsTrackerTest {
     @Test
     public void removingElement() {
         WorldElement element = new Animal(new Vector2d(2, 4));
-        grassFieldsBoundsTracker.addElement(element);
+        boundsTracker.addElement(element);
 
-        grassFieldsBoundsTracker.removeElement(element);
+        boundsTracker.removeElement(element);
 
-        Vector2d lowerLeft = grassFieldsBoundsTracker.lowerLeft();
-        Vector2d upperRight = grassFieldsBoundsTracker.upperRight();
+        Vector2d lowerLeft = boundsTracker.lowerLeft();
+        Vector2d upperRight = boundsTracker.upperRight();
 
         assertEquals(new Vector2d(0, 0), lowerLeft);
         assertEquals(new Vector2d(0, 0), upperRight);
