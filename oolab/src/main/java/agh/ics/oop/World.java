ff2e3569c8b33205fa5a1d84b31243763afee75d
Piotr Karamon <piotrkaramon3@gmail.com>
2023-11-26 23:20:47 +0100

    feat(SimulationEngine): created runAsyncInThreadPool

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -10,7 +10,7 @@ public class World {
     public static void main(String[] options) {
         System.out.println("Start");
 
-        SimulationEngine simulationEngine = new SimulationEngine(getSimulations(500));
+        SimulationEngine simulationEngine = new SimulationEngine(getSimulations(1200));
         simulationEngine.runAsyncInThreadPool();
 
         try {
